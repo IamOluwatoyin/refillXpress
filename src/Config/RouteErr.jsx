56b5e8f6c25fcaa4d54b1afd240d6de733f4deb9
Oluwatoyin Solomon <toyinsolomon4@gmail.com>
2025-10-17 21:46:56 +0100

    collaboration

---
+++ src/Config/RouteErr.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RouteErr = () => {
+  return (
+    <div>
+    404  this page is not found
+    </div>
+  )
+}
+
+export default RouteErr
---
+++ src/Config/Router.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import RouteErr from './RouteErr'
+import HomePage from '../Pages/Home/HomePage'
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+         <Route path="*" element={<RouteErr/>}/>
+         <Route path="/" element={<HomePage/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
