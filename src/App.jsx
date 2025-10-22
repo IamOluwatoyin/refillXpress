cd51f97dba155e00b7d71359d5a2d50225a818a3
Oluwatoyin Solomon <toyinsolomon4@gmail.com>
2025-10-22 11:21:43 +0100

    uploading to get the logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/Router'
+import VendorSignup from './auth/vendor-auth/vendor-signup/vendor-signup'
 const App = () => {
   return (
     <div>
-     <RouterProvider router={router}/>
+     {/* <RouterProvider router={router}/> */}
+     <VendorSignup/>
       
     </div>
   )
