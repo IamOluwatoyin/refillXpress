c655fe5f664852209b504627f16ee867defb9fe2
DanielUtodio <danielletstryagain@gmail.com>
2025-10-23 18:00:27 +0100

    reset

--- src/Auth/reset-password/ResetPassword.jsx
+++ src/Auth/reset-password/ResetPassword.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import "./resetpassword.css"
 
 const ResetPassword = () => {
     return (
-        <div>
-            
+        <div className='reset'>
+            <article className="article">
+                <header className="header">
+                    <h3>RefillXpress</h3>
+                </header>
+                <form className="form">
+                    <div className="form-heading">
+                        <h1>reset password</h1>
+                        <p>Dont't worry, it happens a lot!</p>
+                    </div>
+                    <div className="input-container">
+                        <label htmlFor="email">email address</label>
+                        <div className="input-div">
+                            <input type="text" placeholder='your email here...' />
+                            
+                        </div>
+                    </div>
+                </form>
+            </article>
         </div>
     )
 }
