38bb25d18b8f5e856ba4782dec56efb52494d8f7
Benjamin297 <benjaminuzor782@gmail.com>
2025-11-14 15:05:26 +0100

    error corrected

--- src/Components/ApplicationSuccessPage.jsx
+++ src/Components/ApplicationSuccessPage.jsx
@@ -1,48 +1,46 @@
-import React from "react";
-import styled from "styled-components";
-import { FaCheckCircle } from "react-icons/fa";
-import { useNavigate } from "react-router-dom"; 
+// import React from "react";
+// import styled from "styled-components";
+// import { FaCheckCircle } from "react-icons/fa";
+// import { useNavigate } from "react-router-dom";
 
+// const ApplicationSuccessModal = ({
+//   applicationId = "RDR-FNSL9GD2K",
+//   onComplete,
+// }) => {
+//   const navigate = useNavigate();
 
+//   const handleClose = () => {
+//     if (onComplete) {
+//       onComplete();
+//     } else {
+//       navigate("/");
+//     }
+//   };
 
-const ApplicationSuccessModal = ({
-  applicationId = "RDR-FNSL9GD2K",
-  onComplete,
-}) => {
-  const navigate = useNavigate();
+//   return (
+//     <ModalBackdrop onClick={handleClose}>
+//       <ModalContent onClick={(e) => e.stopPropagation()}>
+//         <CheckIconWrapper>
+//           <CheckIcon />
+//         </CheckIconWrapper>
 
-  const handleClose = () => {
-    if (onComplete) {
-      onComplete();
-    } else {
-      navigate("/"); 
-    }
-  };
+//         <Title>Application Submitted! 🎉</Title>
 
-  return (
-    <ModalBackdrop onClick={handleClose}>
-      <ModalContent onClick={(e) => e.stopPropagation()}>
-        <CheckIconWrapper>
-          <CheckIcon />
-        </CheckIconWrapper>
+//         <Message>
+//           Thank you for applying to become a rider. We've received your
+//           application and will review it within <strong>24–48 hours</strong>.
+//           You'll receive an email notification once your application has been
+//           processed.
+//         </Message>
 
-        <Title>Application Submitted! 🎉</Title>
+//         <ApplicationIdWrapper>
+//           Application ID: <ApplicationId>{applicationId}</ApplicationId>
+//         </ApplicationIdWrapper>
 
-        <Message>
-          Thank you for applying to become a rider. We've received your
-          application and will review it within <strong>24–48 hours</strong>.
-          You'll receive an email notification once your application has been
-          processed.
-        </Message>
+//         <ContinueButton onClick={handleClose}>Go Home</ContinueButton>
+//       </ModalContent>
+//     </ModalBackdrop>
+//   );
+// };
 
-        <ApplicationIdWrapper>
-          Application ID: <ApplicationId>{applicationId}</ApplicationId>
-        </ApplicationIdWrapper>
-
-        <ContinueButton onClick={handleClose}>Go Home</ContinueButton>
-      </ModalContent>
-    </ModalBackdrop>
-  );
-};
-
-export default ApplicationSuccessModal;
+// export default ApplicationSuccessModal;
